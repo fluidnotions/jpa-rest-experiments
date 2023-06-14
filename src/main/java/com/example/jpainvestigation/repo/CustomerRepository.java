82a0081839ea4ae5515fa95338d21b81075dbd31
Justin Robinson <justin.g.robinson@gmail.com>
2023-06-14 22:56:19 +0200

    update readme

--- src/main/java/com/example/jpainvestigation/repo/CustomerRepository.java
+++ src/main/java/com/example/jpainvestigation/repo/CustomerRepository.java
@@ -8,6 +8,8 @@ import java.util.List;
 import java.util.Optional;
 
 public interface CustomerRepository extends CrudRepository<Customer, Integer> {
+
+    List<Customer> findByFirstNameAndEmail(String firstName, String email);
     @Query("SELECT c FROM Customer c WHERE c.firstName = :firstName OR c.email = :email")
     List<Customer> findByFirstNameOrEmail(String firstName, String email);
 
